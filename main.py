842d7279874bef09bec4df138369035bba228e62
Joao Paulo <jotape.hin@gmail.com>
2026-09-20 11:02:35 -0300

    Implementa contagem de pontos e suaviza animação de rolagem

--- main.py
+++ main.py
@@ -1,18 +1,36 @@
 import shutil, keyboard, random, os, time, dados
+from colorama import init, Style, Back, Fore
+
+init(autoreset=True)
 
 largura = shutil.get_terminal_size().columns
 
+total = 0
+
 def rolar(evento):
-  print('\033[H\033[J', end='')
+
+  global total
+
+  print('\033[H', end='')
 
   for n in range(1, 6):
+    print('\033[H', end='')
+           
     print(dados.faces[random.randint(0, 5)])
-    print('\033[H\033[J', end='')
+    print(Style.BRIGHT + 'PONTOS - ' + Fore.RED + f'{total}')
     time.sleep(0.2)
 
-  print(dados.faces[random.randint(0, 5)].center(largura))
+  print('\033[H', end='')
+ 
+  resultado = random.randint(0, 5)
+      
+  total += (resultado + 1)
+
+  print(dados.faces[resultado].center(largura))
+
+  print(Style.BRIGHT + 'PONTOS - ' + Fore.RED + f'{total}')
 
 keyboard.on_press_key('space', rolar)
 
 while True:
-  ...
+  ... 
