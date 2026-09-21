8ef9c9daf5a384d9c8aff87b5a4bb09606c6892a
Joao Paulo <jotape.hin@gmail.com>
2026-09-21 07:31:00 -0300

    Adiciona alguns comentários e centraliza a pontuação

--- main.py
+++ main.py
@@ -3,21 +3,23 @@ from colorama import init, Style, Back, Fore
 
 init(autoreset=True)
 
+# PEGA LARGURA TOTAL DA TELA
 largura = shutil.get_terminal_size().columns
 
 total = 0
 
-def rolar(evento):
-
+def rolar(evento): 
+     
   global total
 
   print('\033[H', end='')
 
+  # GERA ANIMAÇÃO DO DADO "ROLANDO"
   for n in range(1, 6):
     print('\033[H', end='')
            
-    print(dados.faces[random.randint(0, 5)])
-    print(Style.BRIGHT + 'PONTOS - ' + Fore.RED + f'{total}')
+    print(dados.faces[random.randint(0, 5)].center(largura))
+    print((Style.BRIGHT + 'PONTOS - ' + Fore.RED + f'{total}').center(largura))
     time.sleep(0.2)
 
   print('\033[H', end='')
@@ -26,9 +28,10 @@ def rolar(evento):
       
   total += (resultado + 1)
 
+  # IMPRIME RESULTADO FINAL DO DADO
   print(dados.faces[resultado].center(largura))
 
-  print(Style.BRIGHT + 'PONTOS - ' + Fore.RED + f'{total}')
+  print((Style.BRIGHT + 'PONTOS - ' + Fore.RED + f'{total}').center(largura))
 
 keyboard.on_press_key('space', rolar)
 
